1af11e4bdbe96cd0e531ed82e4acb625ab8523d8
YieoN712 <yieon1028@gmail.com>
2024-09-04 11:01:57 +0900

    행렬 곱셈, 덧셈, 뺄셈 코드 추가

--- Warming-up 1/Warming-up 1-1/warmingup.c
+++ Warming-up 1/Warming-up 1-1/warmingup.c
@@ -51,10 +51,84 @@ void main() {
 		}
 		printf("\n");
 	}
+	printf("\n");
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
 			printf("%d  ", matrix2[i][j]);
 		}
 		printf("\n");
+	}
+	printf("\n");
+
+	scanf("%c", &command);
+
+	int sum = 0;
+	switch (command)
+	{
+	case 'm':
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				sum = matrix1[0][j] * matrix2[j][i] + sum;
+			}
+			matrix_result[0][i] = sum;
+			sum = 0;
+		}
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				sum = matrix1[1][j] * matrix2[j][i] + sum;
+			}
+			matrix_result[1][i] = sum;
+			sum = 0;
+		}
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				sum = matrix1[2][j] * matrix2[j][i] + sum;
+			}
+			matrix_result[2][i] = sum;
+			sum = 0;
+		}
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				sum = matrix1[3][j] * matrix2[j][i] + sum;
+			}
+			matrix_result[3][i] = sum;
+			sum = 0;
+		}
+		printf("\n");
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				printf("%d  ", matrix_result[i][j]);
+			}
+			printf("\n");
+		}
+		break;
+	case 'a':
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				matrix_result[i][j] = matrix1[i][j] + matrix2[i][j];
+			}
+		}
+		printf("\n");
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				printf("%d  ", matrix_result[i][j]);
+			}
+			printf("\n");
+		}
+		break;
+	case'd':
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				matrix_result[i][j] = matrix1[i][j] - matrix2[i][j];
+			}
+		}
+		printf("\n");
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				printf("%d  ", matrix_result[i][j]);
+			}
+			printf("\n");
+		}
+		break;
 	}
 }
